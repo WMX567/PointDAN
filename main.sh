e45245f347452c35eb49d6d605548b9981aa3f33
Mengxi Wu <wmx567@Mengxis-Mac-Studio.local>
2025-07-14 16:40:26 -0700

    test

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=32GB
-#SBATCH --time=24:00:00
+#SBATCH --time=00:30:00
 #SBATCH -p nvidia
 #SBATCH --gres=gpu:a100:1
 #SBATCH --output=ms2ver13.out
